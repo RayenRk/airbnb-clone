2f636565c8d4e55eb9bfbad9ad6dfa9a65497943
RayenRk <rakkadrayen@gmail.com>
2024-06-05 16:55:08 +0100

    docker postgres config

---
+++ airbnb-clone-back/src/main/java/org/example/airbnbcloneback/user/domain/Authority.java
@@ -0,0 +1,51 @@
+package org.example.airbnbcloneback.user.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "authority")
+public class Authority implements Serializable {
+
+    @NotNull
+    @Size(max = 50)
+    @Id
+    @Column(length = 50)
+    private String name;
+
+    public @NotNull @Size(max = 50) String getName() {
+        return name;
+    }
+
+    public void setName(@NotNull @Size(max = 50) String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Authority authority = (Authority) o;
+        return Objects.equals(name, authority.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name);
+    }
+
+    @Override
+    public String toString() {
+        return "Authority{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
