92c5b48b6fe39cbce1f24adc7e9c6eaebf6096cb
RayenRk <rakkadrayen@gmail.com>
2024-06-06 18:32:08 +0100

    🟦 Booking Feature Backend

--- airbnb-clone-back/src/main/java/org/example/airbnbcloneback/infrastructure/config/SecurityUtils.java
+++ airbnb-clone-back/src/main/java/org/example/airbnbcloneback/infrastructure/config/SecurityUtils.java
@@ -88,4 +88,5 @@ public class SecurityUtils {
                 extractAuthorityFromClaims(jwtAuthenticationToken.getToken().getClaims()) : authentication.getAuthorities();
         return authorities.stream().map(GrantedAuthority::getAuthority);
     }
+
 }
